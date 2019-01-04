99623a1eb7497002f16550a9f002cbfa45929c4f
Makino <tbk40504@outlook.jp>
2019-01-04 15:34:23 +0900

    テストコミット

--- src/main/java/com/example/demo/domain/service/SampleService.java
+++ src/main/java/com/example/demo/domain/service/SampleService.java
@@ -2,21 +2,27 @@ package com.example.demo.domain.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.domain.dao.SampleDao;
 import com.example.demo.domain.entity.SampleEntity;
-import com.example.demo.domain.repository.SampleRepository;
 
 @Service
 @Transactional
 public class SampleService {
 
 //	@Autowired
-	SampleRepository sampleRepository;
+//	SampleRepository sampleRepository;
+
+	@Autowired
+	SampleDao sampleDao;
 
 	public List<SampleEntity> getSample() {
-		return sampleRepository.selectAll();
+//		return sampleRepository.selectAll();
+
+		return sampleDao.select();
 	}
 
 }
